5411e20ec8e972b1e2af2e67f499d48defe2b0c2
Leonardo Meléndez <leogatgens@gmail.com>
2019-05-24 17:09:18 -0600

    avance de redux ajax

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,4 @@
+import fetch from 'cross-fetch';
 /*
  * action types
  */
@@ -6,15 +7,9 @@ export const CHANGE_COLOR = 'CHANGE_COLOR'
 export const REQUEST_COUNTRIES_BY_CONTINENT = 'REQUEST_COUNTRIES_BY_CONTINENT'
 export const NEXT_COUNTRY = 'NEXT_COUNTRY'
 
-/*
- * other constants
- */
 
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+
 
 /*
  * action creators
@@ -30,4 +25,55 @@ export function RequestContinents(index) {
 
 export function nextCountry(index) {
   return { type: NEXT_COUNTRY, index }
+}
+
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+function requestPosts(subreddit) {
+  return {
+    type: REQUEST_POSTS,
+    subreddit
+  }
+}
+
+function receivePosts(subreddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    subreddit,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
+}
+export function fetchPosts(subreddit) {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return function(dispatch) {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(requestPosts(subreddit))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(
+        response => response.json(),
+        // Do not use catch, because that will also catch
+        // any errors in the dispatch and resulting render,
+        // causing a loop of 'Unexpected batch number' errors.
+        // https://github.com/facebook/react/issues/6895
+        error => console.log('An error occurred.', error)
+      )
+      .then(json =>
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+
+        dispatch(receivePosts(subreddit, json))
+      )
+  }
 }
